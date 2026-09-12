1572366c5811068987e0c51a813b1037dbbee3e3
Manuel Friedmacher <74181220+Friedmacher@users.noreply.github.com>
2026-09-12 10:08:38 +0200

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,19 +2,16 @@
 set -e
 
 echo ""
-echo "✅ User-level configuration started."
+echo "‼️ User-level configuration started."
 
-# Example of a user-level configuration script that runs after the devcontainer is created.
-#terraform version && terraform-docs --version
+echo ""
+echo "Create required directory structure for repositories into ../repos/"
+sudo mkdir -p ../repos
+sudo chown vscode:vscode ../repos
 
-# Create a directory for repositories and set ownership to the vscode user
-#sudo mkdir -p ../repos
-#sudo chown vscode:vscode ../repos
-
-#echo ""
-#echo "Cloning required repositories into ../repos/"
-#git clone https://github.com/manuel-friedmacher/tf_module_build_sa.git ../repos/tf_module_build_sa
-#git clone https://github.com/manuel-friedmacher/tf_module_enable_cf.git ../repos/tf_module_enable_cf
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+sudo chown vscode:vscode "$SCRIPT_DIR/.devcontainer/get_git_repo.sh"
+sudo chmod +x "$SCRIPT_DIR/.devcontainer/get_git_repo.sh"
 
 # echo ""
 # echo "Install Python packages"
